21dc9d76ff649285187c30892c1d539bc2a65168
杨云召 <yangyunzhao@bcbook.cn>
2019-04-13 09:31:07 +0800

    Initial commit

---
+++ baselib/src/main/java/com/example/testbase/Utils.java
@@ -0,0 +1,17 @@
+package com.example.testbase;
+
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+/**
+ * @author yyz (杨云召)
+ * @date 2019/4/13
+ * time   9:17
+ * description
+ */
+public class Utils {
+    public static float dp2px(float dp) {
+
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, Resources.getSystem().getDisplayMetrics());
+    }
+}
